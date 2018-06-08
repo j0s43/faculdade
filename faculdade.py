f3e043e41c0b535b71801cf3cc1b1660ecb168a0
j0s43 <Junior1999@>
2018-06-07 21:38:15 -0300

    Commit inicial

---
+++ faculdade.py
@@ -0,0 +1,74 @@
+# importando as bibliotecas
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+
+# Instanciar a app e o bd
+faculdade = Flask(__name__)
+mysql = MySQL()
+mysql.init_app(faculdade)
+
+# criar uma rota para /
+@faculdade.route('/')
+
+# metodo que responde a rota /
+def index():
+    return render_template('login.html')
+
+# criar uma rota para login
+@faculdade.route('/login', methods=['POST'])
+
+# metodo que responde a rota /login
+def login():
+    # recupera os parametro
+    param_nome = request.form.get('nome')
+    param_senha = request.form.get('senha')
+
+    # conectar no bd
+    faculdade.config['MYSQL_DATABASE_USER'] = 'root'
+    faculdade.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+    faculdade.config['MYSQL_DATABASE_DB'] = 'faculdade'
+
+    # obtendo o cursor
+    cursor = mysql.get_db().cursor()
+
+    # executando o sql
+    cursor.execute(f'SELECT idusuario FROM usuario where nomeusuario = "{param_nome}" and senhausuario = "{param_senha}"')
+    # obter o retorno
+    idusuario = cursor.fetchone()
+    #fechar o cursor
+    cursor.close()
+
+    if idusuario is None:
+        return 'Usuario invalido'
+    else:
+        return render_template('principal.html', nome=param_nome, idusuario=idusuario[0])
+
+
+# rata para /listarturma
+@faculdade.route('/listarTurma')
+# metodo para tratar requesicao /listar turma
+def listar_turmas():
+    # recuperar os parametros
+    id = request.args.get('id')
+
+    # conectar no bd
+    faculdade.config['MYSQL_DATABASE_USER'] = 'root'
+    faculdade.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+    faculdade.config['MYSQL_DATABASE_DB'] = 'faculdade'
+
+    # obtendo o cursor
+    cursor = mysql.get_db().cursor()
+
+    # executar o sql
+    cursor.execute(f'SELECT d.nomedisciplina, d.cursodisciplina, t.nometurma, t.diaturma from disciplina d, turma t, usuario u where u.idusuario = {id} and t.idusuario = u.idusuario and d.iddisciplina = t.iddisciplina')
+
+    #retorno do select
+    turmas = cursor.fetchall()
+
+    #fechar o cursor
+    cursor.close()
+
+    return render_template('turmas.html', id=id, turmas=turmas)
+
+# Iniciar a app
+faculdade.run(debug=True)
